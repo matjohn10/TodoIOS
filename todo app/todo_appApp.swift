b6bc8dc538dc48239a3413b44c4916ed6c97a08d
Mathieu Johnson <mathieujohnson10@gmail.com>
2023-12-22 22:54:33 -0500

    Initial Commit

---
+++ todo app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo app
+//
+//  Created by Mathieu Johnson on 2023-12-22.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
